d58442fd0b2c0a91176bd4e21b9f826461ac01d7
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2019-07-11 16:10:46 +0200

    mysql pool in progress

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path'); // path module to work with directories and files
 var fs = require('fs'); //files module
 var moment = require('moment'); //npm install moment -- for dates and times
 const mysql = require('mysql');
+require('/api/config/credentials.js')
 
 var app = express(); //call express
 
@@ -18,10 +19,8 @@ app.get('*', function(req, res, next) {
     // Prevents an HTML response for API calls
     console.log('get *')
     if (req.path.indexOf('/api/') != -1) {
-      console.log('url has /api/') //1 nuevo
         return next();
-    } console.log('url does not have /api/') //1 nuevo
-    
+    }     
     
     
     fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text) {
@@ -29,43 +28,86 @@ app.get('*', function(req, res, next) {
         
     });
 });
-   
 
-    //testing routes for the issue 28
+//declaration enviroment variables:
+var host= process.env.MYSQL_DB_HOST;
+var username= process.env.MYSQL_DB_USERNAME;
+var password= process.env.MYSQL_DB_PASSWORD;
+var db= process.env.MYSQL_DB_NAME;
+
+var connection = mysql.createPool({
+    connectionLimit:10,
+    host: host,
+    user: username,
+    password: password,
+    database: db, 
+    port: 3000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
+
+});
+
+connection.query("SELECT * FROM Courses", function(error, results, fields){
+    if(error) throw error;
+    res.json(results);
+    console.log('Probando');
+});
+
+connection.getConnection(function (error, connection){
+    if(error){
+        console.log('not connected');
+        throw error;
+    }
+    return connection
+});
+
+
+//testing routes for the issue 28
 app.get('/api/fetchCourses', (req,res) => {
     console.log('fetchCourses')
+    /*connection.getConnection(function(error, connection){
+       if(error){
+        console.log('Error!!');
+        connection.release();
+       } else {*/
+           console.log('Connected!!!')
+           connection.query('SELECT * FROM Courses', function (error, rows, fields){
+            console.log('devuelve datos')
+            res.json(rows)
+            connection.release()
+           })
+       
+    });
+
+
     
-    const connection= mysql.createConnection({
-        host     : 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user     : 'ya3jszaappent36v',
-        password : 'k9r6ns2tezfkrjcg',
-        database : 'sas2dvcw3k2a441b'
-    })
 
-    connection.query("SELECT * FROM Courses", (err, rows, fiels) => {
-        res.json(rows)
-    })
-
-    //res.end()
-})
 
 app.get('/api/fetchCourse/:id', (req,res) => {
     console.log('Fetching courses by id' + req.params.id)
-    const connection= mysql.createConnection({
-        host     : 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user     : 'ya3jszaappent36v',
-        password : 'k9r6ns2tezfkrjcg',
-        database : 'sas2dvcw3k2a441b'
-    })
-  
+
+    const connection= getConnection()
     const courseId = req.params.id
     const queryString ="SELECT * FROM Courses WHERE id = ?"
-    connection.query(queryString, [courseId], (err, rows, fields) => {
-        
-        res.json(rows)
-    })
     
-})
+    
+   
+            connection.query(queryString, [courseId], (err, rows, fields) =>  {
+                res.json(rows)
+            
+          
+          
+            })
+        
+        }),
+      
+   //}
+//})
+  
+    
+
+
 
 
 //message when the server is running
